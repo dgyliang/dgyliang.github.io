0294e29006fc525661af6960eccd2e746f74f225
dgyliang <dgyliang@gmail.com>
2020-12-17 02:20:00 -0800

    somehow learned how to use intersection observer :O

--- js/accordion.js
+++ js/accordion.js
@@ -1,3 +1,5 @@
+// accordion buttons
+
 var accordions = document.getElementsByClassName('accordion');
 
 for (var i=0; i<accordions.length; i++) {
@@ -17,20 +19,4 @@ for (var i=0; i<accordions.length; i++) {
             content.style.maxHeight = content.scrollHeight + 'px';
         }
     }
-}
-
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1}
-  x[slideIndex-1].style.display = "block";
-  setTimeout(carousel, 4000);
-
 }
